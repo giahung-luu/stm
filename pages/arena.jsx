4ac319925bb1724fb7d90d60dddef6367b699629
gnuhzxje <65438928+giahung-luu@users.noreply.github.com>
2022-11-20 11:43:16 +0000

    update arena

--- pages/arena.jsx
+++ pages/arena.jsx
@@ -1,72 +1,76 @@
-import React from "react";
+import {useState, useEffect} from "react";
+import { getQuestions } from "../services/listQuestion";
+import shuffle from "../lib/shuffle";
 
-function Arena() {
+function Arena({questions}) {
+  const [current, setCurrent] = useState(0);
+  const [cQuestion, setCQuestion] = useState(questions[0]);
+  useEffect(()=>{
+    setCQuestion(questions[current])
+  },[current])
+  const nextQuestion = (e) => {
+    if(current <questions.length - 1 )
+     setCurrent((prev)=>prev+1);
+  }
+  const previousQuestion = (e) => {
+    if(current > 0 )
+     setCurrent((prev)=>prev-1);
+  }
   return (
     <div className="Arena">
-      <h2 className="text-center">Arena</h2>
-      <div className="container mt-5">
-        <div className="d-flex justify-content-center row">
-          <div className="col-md-10 col-lg-10">
-            <div className="border">
-              <div className="question bg-white p-3 border-bottom">
-                <div className="d-flex flex-row justify-content-between align-items-center mcq">
+      <h2 className="tw-text-center">Arena</h2>
+      <div className="tw-container tw-mt-5">
+        <div className="tw-flex tw-justify-center row">
+          <div className="col-md-12 col-lg-12">
+            <div className="tw-border">
+              <div className="question tw-bg-gray-700 tw-p-3 border-bottom">
+                <div className="tw-flex flex-row tw-justify-between tw-items-center mcq tw-text-white">
                   <h4>MCQ Quiz</h4>
-                  <span>(5 of 20)</span>
+                  <span>({questions.indexOf(cQuestion)+1} of {questions.length})</span>
                 </div>
               </div>
-              <div className="question bg-white p-3 border-bottom">
-                <div className="d-flex flex-row align-items-center question-title">
+              <div className="question tw-bg-gray-600 tw-p-3 border-bottom">
+                <div className="tw-flex flex-row tw-items-center question-title">
                   <h3 className="text-danger">Q.</h3>
-                  <h5 className="mt-1 ml-2">
-                    Which of the following country has largest population?
+                  <h5 className="tw-mt-1 tw-ml-2">
+                    {cQuestion.title}
                   </h5>
                 </div>
-                <div className="ans ml-2">
+                {
+                  cQuestion.list_answer.map((answer, index)=>(
+                    <div className="ans ml-2" key={index}>
                   <label className="radio">
                     {" "}
-                    <input type="radio" name="brazil" value="brazil" />{" "}
-                    <span>Brazil</span>
+                    <input type="radio" name="brazil" value="a" />{" "}
+                    <span className="tw-text-white">
+                      {answer}
+                    </span>
                   </label>
                 </div>
-                <div className="ans ml-2">
-                  <label className="radio">
-                    {" "}
-                    <input type="radio" name="Germany" value="Germany" />{" "}
-                    <span>Germany</span>
-                  </label>
-                </div>
-                <div className="ans ml-2">
-                  <label className="radio">
-                    {" "}
-                    <input
-                      type="radio"
-                      name="Indonesia"
-                      value="Indonesia"
-                    />{" "}
-                    <span>Indonesia</span>
-                  </label>
-                </div>
-                <div className="ans ml-2">
-                  <label className="radio">
-                    {" "}
-                    <input type="radio" name="Russia" value="Russia" />{" "}
-                    <span>Russia</span>
-                  </label>
-                </div>
+                  ))
+                }
               </div>
-              <div className="d-flex flex-row justify-content-between align-items-center p-3 bg-white">
+              <div className="tw-flex flex-row tw-justify-between tw-items-center p-3 tw-bg-gray-700">
                 <button
-                  className="btn btn-primary d-flex align-items-center btn-danger"
+                  className="btn btn-primary tw-flex tw-items-center btn-danger tw-text-red-600 hover:tw-text-white"
+                  type="button"
+                  onClick={previousQuestion}
+                >
+                  <i className="fa fa-angle-left mt-1 tw-mr-1 "></i>previous
+                </button>
+                {current !== questions.length - 1 ? (<button
+                  className="btn btn-primary tw-flex tw-items-center btn-success tw-text-green-600 hover:tw-text-white"
+                  type="button"
+                  onClick={nextQuestion}
+                >
+                  Next<i className="fa fa-angle-right tw-ml-2"></i>
+                </button>):
+                (<button
+                  className="btn btn-primary tw-flex tw-items-center btn-success tw-text-green-600 hover:tw-text-white"
                   type="button"
                 >
-                  <i className="fa fa-angle-left mt-1 mr-1"></i>&nbsp;previous
-                </button>
-                <button
-                  className="btn btn-primary border-success align-items-center btn-success"
-                  type="button"
-                >
-                  Next<i className="fa fa-angle-right ml-2"></i>
-                </button>
+                  Submit
+                </button>)}
               </div>
             </div>
           </div>
@@ -76,4 +80,15 @@ function Arena() {
   );
 }
 Arena.layout = "default";
+export async function getStaticProps() {
+  const questions = await getQuestions();
+  if (!questions) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { questions },
+  };
+}
 export default Arena;
