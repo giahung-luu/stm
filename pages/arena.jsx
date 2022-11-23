c318486f66b61262477848791b725ed713c94e1f
gnuhzxje <65438928+giahung-luu@users.noreply.github.com>
2022-11-23 14:03:59 +0000

    fix arena

--- pages/arena.jsx
+++ pages/arena.jsx
@@ -5,15 +5,21 @@ import shuffle from "../lib/shuffle";
 function Arena({ questions }) {
   const [current, setCurrent] = useState(0);
   const [cQuestion, setCQuestion] = useState(questions[0]);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
   useEffect(() => {
     setCQuestion(questions[current]);
   }, [current]);
 
-  const nextQuestion = (e) => {
+  const handleChooseAnswer = (index) => {
+    const compare = ["a", "b", "c", "d"];
+    if (compare[index] == cQuestion.right_answer) setScore((prev) => prev + 1);
+    const nextQuestion = cQuestion + 1;
     if (current < questions.length - 1) setCurrent((prev) => prev + 1);
-  };
-  const previousQuestion = (e) => {
-    if (current > 0) setCurrent((prev) => prev - 1);
+    else {
+      setShowScore(true);
+    }
   };
   return (
     <div className="Arena">
@@ -25,51 +31,41 @@ function Arena({ questions }) {
               <div className="question tw-bg-gray-700 tw-p-3 border-bottom">
                 <div className="tw-flex flex-row tw-justify-between tw-items-center mcq tw-text-white">
                   <h4>MCQ Quiz</h4>
-                  <span>
-                    ({questions.indexOf(cQuestion) + 1} of {questions.length})
-                  </span>
+                  {!showScore && (
+                    <span>
+                      ({questions.indexOf(cQuestion) + 1} of {questions.length})
+                    </span>
+                  )}
                 </div>
               </div>
               <div className="question tw-bg-gray-600 tw-p-3 border-bottom">
-                <div className="tw-flex flex-row tw-items-center question-title">
-                  <h3 className="text-danger">Q.</h3>
-                  <h5 className="tw-mt-1 tw-ml-2">{cQuestion.title}</h5>
-                </div>
-                {cQuestion.list_answer.map((answer, index) => (
-                  <div className="ans ml-2" key={index}>
-                    <label className="radio">
-                      {" "}
-                      <input type="radio" name="brazil" value="a" />{" "}
-                      <span className="tw-text-white">{answer}</span>
-                    </label>
+                {showScore ? (
+                  <div className="tw-text-white tw-text-center">
+                    Your score is{" "}
+                    <span className="tw-text-[#e75e8d]">{score}</span> out of{" "}
+                    {questions.length}
                   </div>
-                ))}
-              </div>
-              <div className="tw-flex flex-row tw-justify-between tw-items-center p-3 tw-bg-gray-700">
-                <button
-                  className="btn btn-primary tw-flex tw-items-center btn-danger tw-text-red-600 hover:tw-text-white"
-                  type="button"
-                  onClick={previousQuestion}
-                >
-                  <i className="fa fa-angle-left mt-1 tw-mr-1 "></i>previous
-                </button>
-                {current !== questions.length - 1 ? (
-                  <button
-                    className="btn btn-primary tw-flex tw-items-center btn-success tw-text-green-600 hover:tw-text-white"
-                    type="button"
-                    onClick={nextQuestion}
-                  >
-                    Next<i className="fa fa-angle-right tw-ml-2"></i>
-                  </button>
                 ) : (
-                  <button
-                    className="btn btn-primary tw-flex tw-items-center btn-success tw-text-green-600 hover:tw-text-white"
-                    type="button"
-                  >
-                    Submit
-                  </button>
+                  <>
+                    <div className="tw-flex flex-row tw-items-center question-title">
+                      <h3 className="text-danger">Q.</h3>
+                      <h5 className="tw-mt-1 tw-ml-2">{cQuestion.title}</h5>
+                    </div>
+                    <div className="tw-grid tw-grid-cols-2 tw-gap-6 tw-mt-7">
+                      {cQuestion.list_answer.map((answer, index) => (
+                        <button
+                          className="tw-col-span-1 tw-border-2 tw-border-[#e75e8d] tw-rounded-md tw-py-4 tw-px-6 tw-bg-[#e75e8d] tw-text-white hover:tw-text-[#e75e8d] hover:tw-bg-transparent tw-transition-colors tw-duration-300 tw-ease-in-out first-letter:tw-uppercase"
+                          key={index}
+                          onClick={() => handleChooseAnswer(index)}
+                        >
+                          {answer}
+                        </button>
+                      ))}
+                    </div>
+                  </>
                 )}
               </div>
+              <div className="tw-flex flex-row tw-justify-between tw-items-center p-3 tw-bg-gray-700"></div>
             </div>
           </div>
         </div>
