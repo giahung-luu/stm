8375e6974ed11f587b4bcce415eb87e9c0a1b228
gnuhzxje <65438928+giahung-luu@users.noreply.github.com>
2022-12-01 15:05:43 +0000

    update

--- pages/arena/index.js
+++ pages/arena/index.js
@@ -1,53 +1,42 @@
-import { useCallback } from "react";
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
-function Arena() {
-  const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
-  }, []);
+import { getCharacters } from "../../services/characterService";
+function Arena({ characters }) {
   return (
     <div className="Arena">
       <h2 className="tw-text-center">Arena</h2>
       <div className="tw-grid lg:tw-grid-cols-3 md:tw-grid-cols-2 tw-grid-cols-1 tw-gap-6 tw-mt-6">
-        <div className="tw-col-span-1">
-          <div className="card-container">
-            <div className="card-background">
-              
-              <div id="particles-js">
-                <div className="character-image">
-                  <div className="image">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1741575/test-character.png" />
-                  </div>
-                </div>
-
-                <div className="character-information">
-                  <h2 className="name">Quang Trung</h2>
-                  <div className="description">
-                    <div className="description-text">
-                      <p style={{ color: "Pink", fontWeight: "bold" }}>
-                        Legendary
-                      </p>
-                      <p>Unique character model</p>
-                      <p>Battlepass season 5</p>
+        {characters &&
+          characters.map((item, index) => (
+            <div className="tw-col-span-1 tw-overflow-hidden" key={index}>
+              <div className="card-container">
+                <div className="card-background">
+                  <div id="particles-js">
+                    <div className="character-image">
+                      <div className="image">
+                        <img src={item.img} />
+                      </div>
+                    </div>
+                    <div className="character-information">
+                      <h2 className="name">{item.name}</h2>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          ))}
       </div>
     </div>
   );
 }
 Arena.layout = "default";
-
+export async function getStaticProps() {
+  const characters = await getCharacters();
+  if (!characters) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { characters },
+  };
+}
 export default Arena;
