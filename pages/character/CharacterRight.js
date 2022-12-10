e301d9d40bc07a0571d7f752eab2be95e86116e5
gnuhzxje <65438928+giahung-luu@users.noreply.github.com>
2022-12-10 18:01:45 +0700

    update timeline

--- pages/character/CharacterRight.js
+++ pages/character/CharacterRight.js
@@ -1,17 +1,18 @@
 export default function CharacterRight(props) {
-    const { character } = props;
-    return <div className="item row py-4 my-2">
-        <div className="hover-effect col-lg-10 text-end">
-            <h4>
-                {character.name}
-                <br />
-                <span>
-                    {character.description}
-                </span>
-            </h4>
-        </div>
-        <div className="col-lg-2">
-            <img src={character.img} alt="img" />
-        </div>
+  const { character } = props;
+
+  return (
+    <div className="item row py-4 my-2">
+      <div className="hover-effect col-lg-10 text-end">
+        <h4>
+          {character.name}
+          <br />
+          <span>{character.description}</span>
+        </h4>
+      </div>
+      <div className="col-lg-2">
+        <img src={character.img} alt="img" />
+      </div>
     </div>
+  );
 }
